6ea189f59dbfadcdd41cd653297254f5d63a5fbc
Jens CT Hetland <sharkantos1@gmail.com>
2020-06-14 13:18:46 +0200

    feat: created component ShowMap for showing earth

---
+++ src/views/components/ShowMap.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as THREE from 'three';
+
+class ShowMap extends React.Component {
+    render() {
+        return (
+            <h1>ShowMap not implemented</h1>
+        )
+    }
+}
+
+
+export default ShowMap;
